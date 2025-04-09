13d783eb6ae1108ce123b5e1385e10c046dd4c26
parkermorgan <156543039+parkermorgan@users.noreply.github.com>
2025-04-09 09:16:40 -0600

    Added footer and new buttons

--- src/js/script.js
+++ src/js/script.js
@@ -2,23 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const promptText = document.getElementById("prompt-text");
     const generateBtn = document.getElementById("generate-prompt");
 
-    let allPrompts = [];
+    let scenePrompts = [];
 
     fetch("json/prompts.json")
         .then(response => response.json())
         .then(data => {
-            allPrompts = Object.values(data).flat();
+            scenePrompts = data.scenes || [];
         })
         .catch(error => console.error("Error loading prompts:", error));
 
     generateBtn.addEventListener("click", () => {
-        if (allPrompts.length > 0) {
-            const randomIndex = Math.floor(Math.random() * allPrompts.length);
-            promptText.textContent = allPrompts[randomIndex];
+        if (scenePrompts.length > 0) {
+            const randomIndex = Math.floor(Math.random() * scenePrompts.length);
+            promptText.textContent = scenePrompts[randomIndex];
         } else {
             promptText.textContent = "Error: No prompts available.";
         }
     });
-
-
 });
--- src/js/youtube.js
+++ src/js/youtube.js
@@ -1,15 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
     const videoContainer = document.getElementById("video-container");
     const fetchVideosBtn = document.getElementById("fetch-videos");
+    const filmVideosBtn = document.getElementById("film-videos");
 
     
 
     const API_KEY = 'AIzaSyCVTBA1X8H-eeGsJ79LMMMxT2nS9616SnQ';
     const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 
-    fetchVideosBtn.addEventListener("click", () => {
-        fetchYouTubeVideos("whose line is it anyway");
-    });
+    if (fetchVideosBtn) {
+        fetchVideosBtn.addEventListener("click", () => {
+            fetchYouTubeVideos("whose line is it anyway");
+        });
+    }
+
+    if (filmVideosBtn) {
+        filmVideosBtn.addEventListener("click", () => {
+            fetchYouTubeVideos("Whose line is it anyway guessing game");
+        });
+    }
 
     async function fetchYouTubeVideos(query) {
         const url = `${BASE_URL}?part=snippet&q=${encodeURIComponent(query)}&type=video&maxResults=3&key=${API_KEY}`;
